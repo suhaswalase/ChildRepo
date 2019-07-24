f0dc0358f271b7127c9499e531eefdf1b1ba533a
suhaswalase <suhas.walase@gmail.com>
2019-07-24 14:16:10 +0530

    Push ChildRepo ClassLibrary.csproj

---
+++ ChildRepoClassLibrary/ClassLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public Student()
+        {
+            Id = 10;
+        }
+    }
+}
